82ba548fa4a486b3a62cf6794d491b70f044e7b5
Your Name <contactnikitakazakov@gmail.com>
2019-09-23 15:10:23 -0600

    Add Product qty methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+
+  #QTY SOLD (Run from console. have fun. Product.modify_qty_sold)
+  def self.modify_qty_sold
+    products = Product.all
+    products.each do |product|
+      product.update_attributes(qty_sold: 10)
+    end
+  end
+
+  #Same as above method but with a custom NUMBER.
+  def self.custom_qty_sold(num)
+    products = Product.all
+    products.each do |product|
+      product.update(qty_sold:num)
+    end
+  end
 end
