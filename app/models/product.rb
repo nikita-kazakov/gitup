30ff27b32f7ab201616a81a238f1f3518df072aa
Your Name <contactnikitakazakov@gmail.com>
2019-09-24 17:53:48 -0600

    Active Record Calculations and Scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
 
+  scope :chairs, lambda {where(name:"Chair")}
+  scope :tables, lambda {where(name:"Table")}
+  scope :lamps, lambda {where(name:"Lamp")}
+
+  #scope :price_between, lambda {where(price: 50..100)}
+
+
   #QTY SOLD (Run from console. have fun. Product.modify_qty_sold)
   def self.modify_qty_sold
     products = Product.all
@@ -15,4 +22,13 @@ class Product < ApplicationRecord
       product.update(qty_sold:num)
     end
   end
+
+  def self.update_price_batch
+    Product.in_batches do |batch|
+      sleep 2
+      batch.update_all(inventory: 300)
+    end
+  end
+
+
 end
