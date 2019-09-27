1921811fc436e8527053638647e4cebd49f68fdb
Your Name <contactnikitakazakov@gmail.com>
2019-09-26 22:38:39 -0600

    call backs for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,20 +1,22 @@
 class Category < ApplicationRecord
   has_many :products
 
-  validates :name, presence: true
   validates :prefix, presence: true
+  validates :name, presence: true
+
 
   before_validation :add_default_prefix
 
   def prefix
     "GEN"
   end
+  private
 
-  def add_default_prefix
-    if prefix.blank?
-      puts "Your prefix was blank. I added GEN to it."
-      self.prefix = "GEN"
+    def add_default_prefix
+      if prefix.blank?
+        puts "Your prefix was blank. I added GEN to it."
+        self.prefix = "GEN"
+      end
     end
-  end
 
 end
