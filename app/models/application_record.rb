79fd10cf7c15cef38b64b18bc8d5972265692a6d
Your Name <contactnikitakazakov@gmail.com>
2019-09-30 09:31:51 -0600

    practice around callbacks

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,16 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  private
+
+    def benchmark
+      puts "*** Set Start Time ***"
+      start_time = Time.now
+      yield
+      puts "*** Set End Time ***"
+      end_time = Time.now
+      puts "***Process took: #{end_time - start_time} seconds"
+    end
+
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,22 +1,21 @@
 class Category < ApplicationRecord
   has_many :products
 
-  validates :prefix, presence: true
-  validates :name, presence: true
+  validates :name, :presence => true
+  validates :prefix, :presence => true
 
+  before_validation :add_default_prefix, if: :prefix_missing?
+  #around_save :benchmark
 
-  before_validation :add_default_prefix
-
-  def prefix
-    "GEN"
-  end
   private
 
-    def add_default_prefix
-      if prefix.blank?
-        puts "Your prefix was blank. I added GEN to it."
-        self.prefix = "GEN"
-      end
-    end
+  def prefix_missing?
+    prefix.blank?
+  end
+
+  def add_default_prefix
+      puts "*** Adding default prefix"
+      self.prefix = 'GEN'
+  end
 
 end
